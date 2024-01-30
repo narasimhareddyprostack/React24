7f3afb0995ff1f69ea8072d182ae9c944b47e4a2
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-01-30 13:06:07 +0530

    tst

--- react12/src/Contacts/ContactList.jsx
+++ react12/src/Contacts/ContactList.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const ContactList = () => {
+const ContactList = (props) => {
   return (
     <div>
-        <h1>ContactList Component</h1>
+        <h3>ContactList Component</h3>
+        <pre>{JSON.stringify(props.contacts)}</pre>
+        <table className='table'>
+          <thead className='bg-dark text-white'>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              props.contacts.map((contact)=>{
+                return <tr>
+                  <td> {contact.login.uuid.substr(32)} </td>
+                  <td> {contact.name.first} </td>
+                  <td> {contact.email} </td>
+                </tr>
+              })
+            }
+          </tbody>
+        </table>
     </div>
   )
 }
--- react12/src/Contacts/Contacts.jsx
+++ react12/src/Contacts/Contacts.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ContactList from './ContactList'
 import Details from  './Details'
+import Axios from 'axios'
 const Contacts = () => {
+  let [contacts,setContacts]=useState([])
+  useEffect(()=>{
+    Axios.get('https://gist.githubusercontent.com/narasimhareddyprostack/7e344f346f47bc53a889d78b5258d0c9/raw/56d531cb936d9c79e2417e5d0e5d8c9c876800f2/contactlist')
+         .then((resp)=>{
+          setContacts(resp.data)
+         })
+         .catch(()=>{})
+  },[])
   return <>
     <div className="container">
+      <h3>Contacts</h3>
+      <pre>{JSON.stringify(contacts)}</pre>
       <div className="row">
         <div className="col-8">
-            <ContactList/>
+            {
+              contacts.length > 0 ?  <>
+               <ContactList  contacts={contacts} />
+                                    </>  :<>Nod Datta</>
+            }
+           
         </div>
         <div className="col-4">
             <Details/>

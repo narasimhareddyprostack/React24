b4a96897eaaa5c2ded1588ed5f85044fba45afa4
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-02-01 17:31:23 +0530

    sts

--- react12/src/App.jsx
+++ react12/src/App.jsx
@@ -1,18 +1,13 @@
-import {BrowserRouter} from 'react-router-dom'
-import Navbar from "./Navbar/Navbar"
-import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
-import Home from './Contacts/Home'
-import Contacts from './Contacts/Contacts'
-let App=()=>{
-    return <>
-    <BrowserRouter>
-        <Navbar/>
-
-        <Switch>
-            <Route path="/home" component={Home}/>
-            <Route path="/contacts" component={Contacts}/>
-        </Switch>
-    </BrowserRouter>
-    </>
+import React from 'react'
+import Message from './Message/Message'
+const App = () => {
+  return (
+    <div>
+        <h1>App Component</h1>
+        <hr />
+        <Message/>
+    </div>
+  )
 }
+
 export default App
---
+++ react12/src/Message/Message.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div>
+        <h1>Message Component</h1>
+        <h3>Message : Hello</h3>
+        <button>GM</button>
+        <button>GN</button>
+
+    </div>
+  )
+}
+
+export default Message
